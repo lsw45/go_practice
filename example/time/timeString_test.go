1f2a53c2d38244a099d00823e354f71835867d1a
李世武 <carey.li@cardinfolink.com>
2020-09-14 16:41:25 +0800

    压缩文件

--- example/time/timeString_test.go
+++ example/time/timeString_test.go
@@ -6,21 +6,8 @@ import (
 	"time"
 )
 
-type Module struct {
-	ActiveTime   *time.Time `json:"activeTime,omitempty" gorm:"not null" required:"true" description:"上架时间"`
-	InActiveTime string     `json:"inactiveTime,omitempty" gorm:"column:inactive_time;not null" required:"true" description:"下架时间"`
-	CreatedAt    *time.Time
-}
-
 func TestTimeToString(t *testing.T) {
-
-	now := time.Now()
-	later := now.AddDate(100, 0, 0)
-
-	adv := Module{
-		ActiveTime:   &now,
-		InActiveTime: later.Format("2006-01-02 15:04:05"),
-		CreatedAt:    &now,
-	}
-	fmt.Printf("%s\n%v\n%v\n", adv.ActiveTime.String(), adv.InActiveTime, adv.CreatedAt)
+	ActiveTime := time.Now()
+	InActiveTime := ActiveTime.Format("2006-01-02 15:04:05")
+	fmt.Printf("%s\n%v\n", ActiveTime.String(), InActiveTime)
 }
